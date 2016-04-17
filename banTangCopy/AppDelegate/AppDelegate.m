8fdd7af831c5cdcc0cd2777dcf4ed6735e326854
曾宇 <zengyu@cengyudeMacBook-Pro.local>
2016-04-17 19:10:36 +0800

    位置修改

--- banTangCopy/AppDelegate/AppDelegate.m
+++ banTangCopy/AppDelegate/AppDelegate.m
@@ -35,8 +35,6 @@
         UINavigationController *navVc = [[UINavigationController alloc] initWithRootViewController:vc];
         //添加至tabBar控制器
         [self.tabBarVc addChildViewController:navVc];
-    //添加Resources(我)控制器
-    UIStoryboard *resouSB = [UIStoryboard storyboardWithName:"" bundle:<#(nullable NSBundle *)#>]
     
     //设置window为主窗口
     [self.window makeKeyAndVisible];
